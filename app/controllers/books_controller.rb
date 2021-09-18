373e8aaaddfecc1cd1cb5bb897be9f0ac7b891f6
EC2 Default User <ec2-user@ip-172-31-80-26.ec2.internal>
2021-09-18 07:13:01 +0000

    アップデートアクションの欄の「book」を「@book」

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,11 +25,11 @@ def edit
 end
 
 def update
-  book = Book.find(params[:id])
-  if book.update(book_params)
-    redirect_to book_path(book.id), notice: 'Book was successfully updated.'
-  else                    
-    render ("books/index")
+  @book = Book.find(params[:id])
+  if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
+  else 
+    render :edit
   end
   
 end
